016c8659b288e3838e53be21f22ac67e6c243eb4
Konstantin Stoldt <konstantin.stoldt@talk-point.de>
2016-01-17 21:11:22 +0100

    init package

--- config/app.php
+++ config/app.php
@@ -148,6 +148,8 @@ return [
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
 
+        JeroenG\Packager\PackagerServiceProvider::class,
+
         /*
          * Application Service Providers...
          */
