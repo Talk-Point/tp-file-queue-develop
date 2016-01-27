47b798fce541e5c4a290ffebddd4c9f46edf4725
Konstantin Stoldt <konstantin.stoldt@talk-point.de>
2016-01-27 21:34:10 +0100

    Implement nextrun in cron

--- tests/Unit-Tests/TaskModelTest.php
+++ tests/Unit-Tests/TaskModelTest.php
@@ -24,4 +24,21 @@ class TaskModelTest extends TestCase
         $this->assertNotNull($o);
         $this->assertEquals('data', $o->string);
     }
+
+    public function test_cron_tasks()
+    {
+        /** @var \TPTaskRunner\Models\Task $task_1 */
+        $task_1 = factory(\TPTaskRunner\Models\Task::class)->create();
+        $task_1->next_run_at = \Carbon\Carbon::now()->addDay(1);
+        $task_1->save();
+        /** @var \TPTaskRunner\Models\Task $task_2 */
+        $task_2 = factory(\TPTaskRunner\Models\Task::class)->create();
+
+
+        $query_1_count = \TPTaskRunner\Models\Task::cron()->count();
+        $query_2_count = \TPTaskRunner\Models\Task::cron(\Carbon\Carbon::now()->addDay(2))->count();
+
+        $this->assertNotEquals($query_1_count, $query_2_count);
+        $this->assertTrue($query_1_count < $query_2_count);
+    }
 }
